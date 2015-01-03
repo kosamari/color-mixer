781a04dd660642b2b4231b2fede65a95dec3646b
kosamari <mariko.kosaka@gmail.com>
2015-01-03 03:58:21 -0500

    adding other color functions

--- sasscolor.js
+++ sasscolor.js
@@ -29,7 +29,6 @@
         }
 
         this.hsl = function(h,s,l){
-            console.log(hslaval)
             if(arguments.length<=0){return hslaval.slice(0,3);}
             this.hsla(h,s,l,1)
         }
@@ -67,6 +66,8 @@
 
     // color manipulation methods
     // 'color' is an each instance of Color.
+
+    // value getters
     c_c.red = function(color){
         return color.hex().substr(1,2);
     }
@@ -91,94 +92,6 @@
         return color.hsl()[3];
     }
 
-    c_c.invert = function(color){
-        var inverted = color.rgba().map(function(c,i){
-            if(i === 3) { return c; }
-            return 255 - c;
-        });
-
-        return {rgb: inverted.slice(0,3),
-                rgba: inverted,
-                hex: color.rgbToHEX(inverted),
-                hsl: rgbToHSL(inverted),
-                hsla: rgbToHSL(inverted).concat([inverted[3]])};
-    }
-
-    c_c.adjust_hue = function(color,deg){
-        var rgb,
-            base = color.hsla().slice(0),
-            diff = 360 - (base[0]+deg);
-
-        base[0] = diff > 360 ? 360 - (diff%360) : Math.abs(diff%360);
-        rgb = hslToRGB(base);
-
-        return {rgb: rgb,
-                rgba: rgb.concat(base[3]),
-                hex: rgbToHEX(rgb),
-                hsl: base.slice(0,3),
-                hsla: base};
-    }
-
-    c_c.complement = function(color){
-        return color.adjust_hue(180)
-    }
-
-    c_c.saturate = function(color,deg){
-        var rgb,
-            base = color.hsla().slice(0);
-        base[1] = Math.min(base[1] + deg, 100);
-        rgb = hslToRGB(base);
-
-        return {rgb: rgb,
-                rgba: rgb.concat(base[3]),
-                hex: rgbToHEX(rgb),
-                hsl: base.slice(0,3),
-                hsla: base};
-    }
-
-    c_c.desaturate = function(color,deg){
-        var rgb,
-            base = color.hsla().slice(0);
-        base[1] = Math.max(base[1] - deg, 0);
-        rgb = hslToRGB(base);
-
-        return {rgb: rgb,
-                rgba: rgb.concat(base[3]),
-                hex: rgbToHEX(rgb),
-                hsl: base.slice(0,3),
-                hsla: base};
-    }
-
-    c_c.grayscale = function(color){
-        return color.desaturate(100)
-    }
-
-    c_c.lighten = function(color,deg){
-        var rgb,
-            base = color.hsla().slice(0);
-        base[2] = Math.min(base[2] + deg, 100);
-        rgb = hslToRGB(base);
-
-        return {rgb: rgb,
-                rgba: rgb.concat(base[3]),
-                hex: rgbToHEX(rgb),
-                hsl: base.slice(0,3),
-                hsla: base};
-    }
-
-    c_c.darken = function(color,deg){
-        var rgb,
-            base = color.hsla().slice(0);
-        base[2] = Math.max(base[2] - deg, 0);
-        rgb = hslToRGB(base);
-
-        return {rgb: rgb,
-                rgba: rgb.concat(base[3]),
-                hex: rgbToHEX(rgb),
-                hsl: base.slice(0,3),
-                hsla: base};
-    }
-
     c_c.alpha = function(color){
         return color.rgba()[3];
     }
@@ -187,30 +100,283 @@
         return this.alpha(color);
     }
 
-    c_c.opacify = function(color,deg){
-        var a = Math.round(Math.min(this.alpha(color) + deg, 1)*100)/100;
+    // RGB
+    c_c.invert = function(color){
+        var inverted = color.rgba().map(function(c,i){
+            if(i === 3) { return c; }
+            return 255 - c;
+        });
 
+        return {rgb: inverted.slice(0,3),
+                rgba: inverted,
+                hex: rgbToHEX(inverted),
+                hsl: rgbToHSL(inverted),
+                hsla: rgbToHSL(inverted).concat([inverted[3]])};
+    }
+
+    c_c.adjust_red = function(color,deg,scale){
+        var hsla,
+            rgba = color.rgba().slice(0);
+
+        if(deg){
+            rgba[0] = Math.max(Math.min(rgba[0] + deg, 255),0);
+        }else{
+            scale>=0
+            ? rgba[0] = Math.floor((255-rgba[0])*scale/100+rgba[0])
+            : rgba[0] = Math.floor(rgba[0]*Math.abs(scale)/100)
+        }
+
+        hsla = rgbToHSL(rgba);
+        return {rgb: rgba.slice(0,3),
+                rgba: rgba,
+                hex: rgbToHEX(rgba),
+                hsl: hsla.slice(0,3),
+                hsla: hsla};
+    }
+
+    c_c.adjust_green = function(color,deg,scale){
+        var hsla,
+            rgba = color.rgba().slice(0);
+
+        if(deg){
+            rgba[1] = Math.max(Math.min(rgba[1] + deg, 255),0);
+        }else{
+            scale>=0
+            ? rgba[1] = Math.floor((255-rgba[1])*scale/100+rgba[1])
+            : rgba[1] = Math.floor(rgba[1]*Math.abs(scale)/100)
+        }
+
+        hsla = rgbToHSL(rgba);
+        return {rgb: rgba.slice(0,3),
+                rgba: rgba,
+                hex: rgbToHEX(rgba),
+                hsl: hsla.slice(0,3),
+                hsla: hsla};
+    }
+
+    c_c.adjust_blue = function(color,deg,scale){
+        var hsla,
+            rgba = color.rgba().slice(0);
+
+        if(deg){
+            rgba[2] = Math.max(Math.min(rgba[2] + deg, 255),0);
+        }else{
+            scale>=0
+            ? rgba[2] = Math.floor((255-rgba[2])*scale/100+rgba[2])
+            : rgba[2] = Math.floor(rgba[2]*Math.abs(scale)/100)
+        }
+
+        hsla = rgbToHSL(rgba);
+        return {rgb: rgba.slice(0,3),
+                rgba: rgba,
+                hex: rgbToHEX(rgba),
+                hsl: hsla.slice(0,3),
+                hsla: hsla};
+    }
+
+    // HUE
+    c_c.adjust_hue = function(color,deg){
+        var rgb,
+            hsla = color.hsla().slice(0),
+            diff = 360 - (hsla[0]+deg);
+
+        hsla[0] = diff > 360 ? 360 - (diff%360) : Math.abs(diff%360);
+        rgba = hslToRGB(hsla);
+
+        return {rgb: rgba.slice(0,3),
+                rgba: rgba,
+                hex: rgbToHEX(rgba),
+                hsl: hsla.slice(0,3),
+                hsla: hsla};
+    }
+
+    c_c.complement = function(color){
+        return color.adjust_hue(180)
+    }
+
+    // SATURATION
+    c_c.adjust_saturation = function(color,deg,scale){
+        var rgb,
+            hsla = color.hsla().slice(0);
+
+        if(deg){
+            hsla[1] = Math.max(Math.min(hsla[1] + deg, 100),0);
+        }else{
+            scale>=0
+            ? hsla[1] = Math.floor((100-hsla[1])*scale/100+hsla[1])
+            : hsla[1] = Math.floor(hsla[1]*Math.abs(scale)/100)
+        }
+
+        rgba = hslToRGB(hsla);
+
+        return {rgb: rgba.slice(0,3),
+                rgba: rgba,
+                hex: rgbToHEX(rgba),
+                hsl: hsla.slice(0,3),
+                hsla: hsla};
+    }
+
+    c_c.saturate = function(color,deg){
+        return this.adjust_saturation(color,deg)
+    }
+
+    c_c.desaturate = function(color,deg){
+        return this.adjust_saturation(color,-deg)
+    }
+
+    c_c.grayscale = function(color){
+        return this.adjust_saturation(color,100)
+    }
+
+    // LIGHTNESS
+     c_c.adjust_lightness = function(color,deg,scale){
+        var rgb,
+            hsla = color.hsla().slice(0);
+
+        if(deg){
+            hsla[2] = Math.max(Math.min(hsla[2] + deg, 100),0);
+        }else{
+            scale>=0
+            ? hsla[2] = Math.floor((100-hsla[2])*scale/100+hsla[2])
+            : hsla[2] = Math.floor(hsla[2]*Math.abs(scale)/100)
+        }
+
+        rgba = hslToRGB(hsla);
+
+        return {rgb: rgba.slice(0,3),
+                rgba: rgba,
+                hex: rgbToHEX(rgba),
+                hsl: hsla.slice(0,3),
+                hsla: hsla};
+    }
+
+    c_c.lighten = function(color,deg){
+        return this.adjust_lightness(color, deg)
+    }
+
+    c_c.darken = function(color,deg){
+        return this.adjust_lightness(color, -deg)
+    }
+
+    // ALPHA
+    c_c.adjust_alpha = function(color,deg,scale){
+        var rgba = color.rgba().slice(0),
+            hsla = color.hsla().slice(0),
+            a;
+
+        if(deg){
+            a = Math.round(Math.max(Math.min(this.alpha(color) + deg, 1),0)*100)/100;
+        }else{
+            scale>=0
+            ? a = Math.floor((1-this.alpha(color))*scale+(this.alpha(color)*100))/100
+            : a = Math.floor(this.alpha(color)*Math.abs(scale))/100
+        }
+
+        rgba[3] = a;
+        hsla[3] = a
         return {
-            rgba: color.rgb().concat([a]),
-            hsla: color.hsl().concat([a])
+            rgba: rgba,
+            hsla: hsla
         };
+    }
+
+    c_c.opacify = function(color,deg){
+        return this.adjust_alpha(color,deg)
     }
 
     c_c.transparentize = function(color,deg){
-        var a = Math.round(Math.max(this.alpha(color) - deg, 0)*100)/100;
-
-        return {
-            rgba: color.rgb().concat([a]),
-            hsla: color.hsl().concat([a])
-        };
+        return this.adjust_alpha(color,-deg)
     }
 
     c_c.fade_in = function(color,deg){
-        return this.opacify(color,deg);
+        return this.adjust_alpha(color,deg)
     }
 
     c_c.fade_out = function(color,deg){
-        return this.transparentize(color,deg);
+        return this.adjust_alpha(color,-deg)
+    }
+
+    // OTHER COLOR FUNCTIONS
+    c_c.adjust_color = function(color,opt){
+        var rgba = color.rgba().slice(0),
+            hsla = color.hsla().slice(0);
+        var operations = {
+            red: function(deg){rgba[0] = this.adjust_red(color,deg).rgba[0]},
+            green: function(deg){rgba[1] = this.adjust_green(color,deg).rgba[1]},
+            blue: function(deg){rgba[2] = this.adjust_blue(color,deg).rgba[2]},
+            hue: function(deg){hsla[0] = this.adjust_hue(color,deg).hsla[0]},
+            saturation: function(deg){hsla[1] = this.adjust_saturation(color,deg).hsla[1]},
+            lightoness: function(deg){hsla[2] = this.adjust_lightness(color,deg).hsla[2]},
+            alpha: function(deg){
+                    var a =  this.adjust_alpha(color,deg).hsla[3];
+                    rgba[3] = a;
+                    hsla[3] = a;
+                }
+        }
+
+        Object.keys(opt).forEach(function(key){
+            operations[key].call(this,opt[key]);
+        },this);
+
+        return {rgb: rgba.slice(0,3),
+                rgba: rgba,
+                hex: rgbToHEX(rgba),
+                hsl: hsla.slice(0,3),
+                hsla: hsla};
+    }
+
+    c_c.scale_color = function(color,opt){
+        var rgba = color.rgba().slice(0),
+            hsla = color.hsla().slice(0);
+        var operations = {
+            red: function(scale){rgba[0] = this.adjust_red(color,null,scale).rgba[0]},
+            green: function(scale){rgba[1] = this.adjust_green(color,null,scale).rgba[1]},
+            blue: function(scale){rgba[2] = this.adjust_blue(color,null,scale).rgba[2]},
+            saturation: function(scale){hsla[1] = this.adjust_saturation(color,null,scale).hsla[1]},
+            lightness: function(scale){hsla[2] = this.adjust_lightness(color,null,scale).hsla[2]},
+            alpha: function(scale){
+                    var a =  this.adjust_alpha(color,null,scale).hsla[3];
+                    rgba[3] = a;
+                    hsla[3] = a;
+                }
+        }
+
+        Object.keys(opt).forEach(function(key){
+            operations[key].call(this,opt[key]);
+        },this);
+
+        return {rgb: rgba.slice(0,3),
+                rgba: rgba,
+                hex: rgbToHEX(rgba),
+                hsl: hsla.slice(0,3),
+                hsla: hsla};
+    }
+
+    c_c.change_color = function(color,opt){
+        var rgba = color.rgba().slice(0),
+            hsla = color.hsla().slice(0);
+        var operations = {
+            red: function(val){rgba[0] = val},
+            green: function(val){rgba[1] = val},
+            blue: function(val){rgba[2] = val},
+            hue: function(val){hsla[0] = val},
+            saturation: function(val){hsla[1] = val},
+            lightness: function(val){hsla[2] = val},
+            alpha: function(val){
+                    rgba[3] = val;
+                    hsla[3] = val;
+                }
+        }
+
+        Object.keys(opt).forEach(function(key){
+            operations[key].call(this,opt[key]);
+        },this);
+
+        return {rgb: rgba.slice(0,3),
+                rgba: rgba,
+                hex: rgbToHEX(rgba),
+                hsl: hsla.slice(0,3),
+                hsla: hsla};
     }
 
     // Conversion Functions
